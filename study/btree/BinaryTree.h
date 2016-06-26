2af0468162ae44edf0a290844e3fc50d90b65cdc
pqy <qypan1994@163.com>
2016-06-26 16:12:59 +0800

    fix

--- study/btree/BinaryTree.h
+++ study/btree/BinaryTree.h
@@ -9,6 +9,8 @@ public:
 	~BinaryTree();
 	BinaryTree(const BinaryTree &);
 	BinaryTree& operator = (const BinaryTree &);
+	friend ostream & operator << (ostream &os,
+			const BinaryTree<elemType> &bt);
 	void insert(const elemType &elem);
 	void remove(const elemType &elem);
 	void preorder(ostream &os = cout){preorder(_root, os);}
@@ -24,8 +26,16 @@ private:
 	void preorder(BTnode<elemType> *pt, ostream &os) const;
 	void inorder(BTnode<elemType> *pt, ostream &os) const;
 	void postorder(BTnode<elemType> *pt, ostream &os) const;
-	void display_val(BTnode<elemType> *pt, ostream &os);
+	void display_val(BTnode<elemType> *pt, ostream &os) const;
 };
+
+template <typename elemType>
+inline ostream &
+operator << (ostream &os, const BinaryTree<elemType> &bt){
+	os << "Tree: " << endl;
+	bt.print(os);
+	return os;
+}
 
 template <typename elemType>
 void BinaryTree<elemType>::
@@ -81,7 +91,7 @@ inline void
 BinaryTree<elemType>::
 remove(const elemType &elem){
 	if(_root){
-		if(_root->val == elem)
+		if(_root->_val == elem)
 			remove_root();
 		else
 			_root->remove_value(elem, _root);
@@ -91,6 +101,57 @@ remove(const elemType &elem){
 template <typename elemType>
 inline void
 BinaryTree<elemType>::
-display_val(BTnode<elemType> *pt, ostream &os){
+display_val(BTnode<elemType> *pt, ostream &os) const{
 	os << pt->_val << ' ';
+}
+
+template <typename elemType>
+void BinaryTree<elemType>::
+remove_root(){
+	if(!_root) return;
+	BTnode<elemType> *tmp = _root;
+	if(_root->_rchild){
+		_root = _root->_rchild;
+		if(tmp->_lchild){
+			BTnode<elemType> *lc = tmp->_lchild;
+			BTnode<elemType> *newlc = _root->_lchild;
+			if(!newlc)
+				_root->_lchild = lc;
+			else
+				BTnode<elemType>::lchild_leaf(lc, newlc);
+		}
+	}
+	else 
+		_root = _root->_lchild;
+	delete tmp;
+}
+
+template <typename elemType>
+void BinaryTree<elemType>::
+preorder(BTnode<elemType> *pt, ostream &os) const{
+	if(pt){
+		display_val(pt, os);
+		if(pt->_lchild) preorder(pt->_lchild, os);
+		if(pt->_rchild) preorder(pt->_rchild, os);
+	}
+}
+
+template <typename elemType>
+void BinaryTree<elemType>::
+inorder(BTnode<elemType> *pt, ostream &os) const{
+	if(pt){
+		if(pt->_lchild) inorder(pt->_lchild, os);
+		display_val(pt, os);
+		if(pt->_rchild) inorder(pt->_rchild, os);
+	}
+}
+
+template <typename elemType>
+void BinaryTree<elemType>::
+postorder(BTnode<elemType> *pt, ostream &os) const{
+	if(pt){
+		if(pt->_lchild) postorder(pt->_lchild, os);
+		if(pt->_rchild) postorder(pt->_rchild, os);
+		display_val(pt, os);
+	}
 }
--- study/btree/main.cpp
+++ study/btree/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main()
 {
 	BinaryTree<string> bt;
-#if 0
 	bt.insert("Piglet");
 	bt.insert("Eeyore");
 	bt.insert("Roo");
@@ -14,8 +13,43 @@ int main()
 	bt.insert("Chris");
 	bt.insert("Pooh");
 	bt.insert("Kanga");
+
+	bt.inorder();
+	cout << endl;
+
+	bt.remove("Piglet");
+
 	bt.preorder();
-#endif
+	cout << endl;
+
+	bt.remove("Pooh");
+
+	bt.preorder();
+	cout << endl;
+
+	bt.remove("Roo");
+
+	bt.preorder();
+	cout << endl;
+
+	bt.remove("Eeyore");
+
+	bt.preorder();
+	cout << endl;
+
+	bt.remove("Tigger");
+
+	bt.preorder();
+	cout << endl;
+
+	bt.remove("Chris");
+
+	bt.preorder();
+	cout << endl;
+
+	bt.remove("Kanga");
+
+	bt.preorder();
 	cout << endl;
 	return 0;
 }
